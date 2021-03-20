ca0a0f8f38095925f88f386f55852bcec111dcf2
KondeU <>
2021-03-20 12:06:27 +0800

    Demo sources.

---
+++ Shaders.h
@@ -0,0 +1,11 @@
+#include <string>
+
+const std::string SHADER_VS
+(R"SHADER(
+@SHADER_VS@
+)SHADER");
+
+const std::string SHADER_FS
+(R"SHADER(
+@SHADER_FS@
+)SHADER");
---
+++ run_cmake.bat
@@ -0,0 +1,6 @@
+PATH=D:\CMake\cmake-3.16.0-win64-x64\bin;%PATH%
+mkdir build
+cd build
+cmake ..
+cd ..
+pause
